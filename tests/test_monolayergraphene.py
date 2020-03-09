0a1d5dcf7b38eaa3acda0d196661329f929e5529
gholdman1 <gholdman@protonmail.com>
2020-03-09 09:29:53 -0500

    Add tests for Monolayer

--- tests/test_monolayergraphene.py
+++ tests/test_monolayergraphene.py
@@ -3,11 +3,24 @@ import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from graphenemodeling.graphene import BaseGraphene
+from graphenemodeling.graphene import Monolayer
 from scipy import constants as sc
+import numpy as np
 
 
 class TestMonolayerGraphene:
 
-	def test_constants(self):
-		pass
+	def test_kFermi_LowEnergy(self):
+		mlg = Monolayer()
+
+		eF = 0.4 * sc.elementary_charge
+		kF = mlg.kFermi(eF,model='LowEnergy')
+
+		assert np.isclose(kF,670690811.5358821)
+
+	def test_DiracFermionDispersion_LowEnergy(self):
+		mlg = Monolayer()
+
+		eF = 0.4 * sc.elementary_charge
+		kF = mlg.kFermi(eF,model='LowEnergy')
+		assert mlg.DiracFermionDispersion(kF,'LowEnergy')/sc.elementary_charge == 0.4
