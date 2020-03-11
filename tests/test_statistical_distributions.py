9214687d6874901a8d00cec9d1bc14d218c4c475
gholdman1 <gholdman@protonmail.com>
2020-03-11 12:19:04 -0500

    Add test for statistical_distributions.BoseEinstein

--- tests/test_statistical_distributions.py
+++ tests/test_statistical_distributions.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import graphenemodeling.statistical_distributions as sd
 from scipy import constants as sc
+import numpy as np
 
 
 class TestFermiDirac:
@@ -15,3 +16,13 @@ class TestFermiDirac:
 		assert fd == 0.5
 
 		
+class TestBoseEinstein:
+
+	def test_single_value(self):
+		kB = sc.k
+		T=300
+		E = kB*T
+
+
+		be = sd.BoseEinstein(E,T)
+
