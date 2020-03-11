1cb80726ae68c4620e77fd508863fcf9ae34aa6f
G Holdman <gholdman@protonmail.com>
2020-03-11 06:45:59 -0500

    DOC: Improve description.

--- graphenemodeling/graphene/__init__.py
+++ graphenemodeling/graphene/__init__.py
@@ -6,7 +6,7 @@ Graphene (:mod:`graphenemodeling.graphene`)
 .. currentmodule:: graphenemodeling.graphene
 
 GrapheneModeling's ``graphene`` is the main module of the package.
-The code here replicates literature on the material graphene.
+The code here replicates literature on the material graphene. Every single function must include an example which demonstrates that it replicates a resource found in the literature. 
 
 Monolayer
 =========
