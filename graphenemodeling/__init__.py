15a2b9837284cf2179500814fcc60a3969226303
G Holdman <gholdman@protonmail.com>
2020-03-10 20:25:39 -0500

    Remove import call in __init__

--- graphenemodeling/__init__.py
+++ graphenemodeling/__init__.py
@@ -1,8 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.animation as animation
-
-from scipy import integrate, optimize
-
 __version__='1.0.2'
