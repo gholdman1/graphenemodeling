e50fde056850bef59e40c18e12d5e33941a98724
gholdman1 <gholdman@protonmail.com>
2020-03-18 07:28:23 -0500

    API: Add tunneling submodule.

---
+++ graphenemodeling/experiments/tunneling.py
@@ -0,0 +1,17 @@
+"""
+==================================================
+Tunneling (:mod:`experiments.tunneling`)
+==================================================
+
+
+
+"""
+
+class TunnelingExperiment:
+
+	def __init__(d1,d2,eps1,eps2):
+
+		self.d1 = d1
+		self.d2 = d2
+		self.eps1 = eps1
+		self.eps2 = eps2
